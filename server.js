ec669a9e652896da901cc747a2e228792c89b9eb
Virmerson Bento dos Santos <virmerson@hightechcursos.com.br>
2020-06-20 13:29:47 -0400

    Ciar um Serverx

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 const express = require('express')
-const app = express()
+
 const port = process.env.PORT || 3000
 const path = require("path")
 const usuarioRoutes = require("./routes/usuario-routes")
 const ManageDB = require("./db/ManageDB")
 
-ManageDB.connect()
-//ManageDB.close()
+class Server {
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(usuarioRoutes)
+    constructor() {
+        this.app = express()
+    }
 
 
-app.listen(port, function () {
-    console.log(`Server running at http://localhost:${port}/`);
-})
+    start() {
+        //middlewarres
+        this.app.use(express.json())
+        this.app.use(express.static(path.join(__dirname, 'public')))
+
+        //Rotas
+        this.app.use(usuarioRoutes)
+
+        //porta
+        this.app.listen(port, function () {
+            console.log(`Server running at http://localhost:${port}/`);
+        })
+
+        //Conexao com banco
+
+        this.conexao()
+
+
+    }
+
+    conexao() {
+        ManageDB.connect()
+        //ManageDB.close()
+    }
+
+
+}
+
+
+new Server().start()
